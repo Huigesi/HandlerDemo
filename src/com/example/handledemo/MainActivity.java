243228f32d5986dc6277943851f1db992612b1fd
huigesi <791339970@qq.com>
2017-11-23 22:36:01 +0800

    s

---
+++ src/com/example/handledemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.handledemo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+	private TextView tv1;
+	private Handler handler=new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+        tv1=(TextView)findViewById(R.id.textview1);
+        new Thread(){
+        	public void run(){
+        		try {
+        			handler.post(new Runnable() {
+						@Override
+						public void run() {
+							tv1.setText("update thread");
+						}
+					});
+        			
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        	}
+        }.start();
+
+       
+    }
+
+
+}
